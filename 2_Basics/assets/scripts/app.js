f2edff4e095b0f834553711d2b0753760b563498
Yaroslav <boyaal24@gmail.com>
2020-06-09 23:30:02 +0300

    add game with words

--- 2_Basics/assets/scripts/app.js
+++ 2_Basics/assets/scripts/app.js
@@ -142,10 +142,141 @@ showThis(4, 5); */
 
 obj.sum(); */
 
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-currentResult = currentResult + 10;
+function add(num1, num2) {
+	const result = num1 + num2;
+	alert();
+}
 
-let calculationDescription = '(0 + 10) * 3/2  -1';
 
-outputResult(currentResult, 'hello');
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2  - 1`;
+
+outputResult(currentResult, calculationDescription);
+
+let score = {
+	'Fedor': 0,
+	'Petya': 0
+}
+
+score.Fedor += 100;
+score.Petya += 50;
+
+
+/* let myCrazyObject = {
+	"name": "Object",
+	"some array": [7, 9, {
+		purpose: 'someone',
+		number: 123
+	}, 3.3],
+	"random animal": "Banana shark"
+};
+
+myCrazyObject["some array"][2].number;
+
+
+for (let x = 3; x <= 10000; x = x * 3) {
+	console.log(x);
+
+}
+
+let x = 3;
+
+while (x <= 10000) {
+	console.log(x);
+	x = x * 3;
+} */
+
+/* let animals = ["cat", "fish", "Lemur", "varan"];
+let newAnimals = [];
+for (let i = 0; i < animals.length; i++) {
+
+	newAnimals[i] = animals[i] + '- beautiful animal';
+	console.log(newAnimals[i]);
+} */
+
+/* let alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыэюя";
+
+let randomString = '';
+
+let stringLength = 6;
+
+while (randomString.length < stringLength) {
+	randomString += alphabet[Math.floor(Math.random() * alphabet.length)];
+}
+
+console.log(randomString); */
+
+/* let input = 'JavaScript is awesome';
+
+let output = '';
+
+for (let i = 0; i < input.length; i++) {
+	if (input[i] === 'a') {
+		output += '4'
+	} else if (input[i] === 'e') {
+		output += '3'
+	} else if (input[i] === 'i') {
+		output += '1'
+	} else if (input[i] === 'o') {
+		output += '0'
+	} else {
+		output += input[i];
+	}
+}
+
+console.log(output); */
+
+//let name = prompt("What is your name?");
+// Create Game//
+
+//Create random word
+let words = [
+	"programm",
+	"makaka",
+	"beautiful",
+	"pancake"
+];
+
+//create final array
+
+let word = words[Math.floor(Math.random() * words.length)];
+
+let answerArray = [];
+for (let i = 0; i < word.length; i++) {
+	answerArray[i] = "_";
+}
+
+let remainingLetters = word.length;
+let guesses = 20
+
+while (remainingLetters > 0 && guesses > 0) {
+	alert(answerArray.join(" "));
+	let guess = prompt("Please, choise a letter or press Cancel");
+	if (guess === null || guess == 1) {
+		break;
+	} else if (guess.length !== 1) {
+		alert("Please, insert letter")
+	} else {
+		guesses--;
+		guess = guess.toLowerCase();
+		for (let j = 0; j < word.length; j++) {
+			if (word[j] === guess) {
+				answerArray[j] = guess;
+				remainingLetters--;
+			}
+		}
+	}
+}
+
+
+
+alert(answerArray.join(" "));
+if (guesses > 0) {
+	alert("Very good! You are a winner" + word);
+} else {
+	alert("Very bad! You are lose" + word);
+}
