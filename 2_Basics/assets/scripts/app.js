36f4f367dd5e0f91df3ef924420e00e26448de58
Yaroslav <boyaal24@gmail.com>
2020-06-12 22:24:26 +0300

    add fuction to calc  with restracting code

--- 2_Basics/assets/scripts/app.js
+++ 2_Basics/assets/scripts/app.js
@@ -144,14 +144,51 @@ obj.sum(); */
 
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
+
+function getUserNumberInput() {
+	return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+	const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+	outputResult(currentResult, calcDescription);
+}
 
 function add() {
-	currentResult = currentResult + userInput.value;
-	outputResult(currentResult, '');
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult += enteredNumber;
+	createAndWriteOutput('+', initialResult, enteredNumber);
+	logEntries.push(enteredNumber);
+	console.log(logEntries[0]);
+}
+
+function subtract() {
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult -= enteredNumber;
+	createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult *= enteredNumber;
+	createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult /= enteredNumber;
+	createAndWriteOutput('/', initialResult, enteredNumber);
 }
 
 addBtn.addEventListener('click', add);
-
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 /* let score = {
 	'Fedor': 0,
