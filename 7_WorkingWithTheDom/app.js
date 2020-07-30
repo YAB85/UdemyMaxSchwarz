1b7a2810835561c0674588a3146a6106be2a5ae8
Yaroslav <boyaal24@gmail.com>
2020-07-30 16:57:44 +0300

    add section about dom

---
+++ 7_WorkingWithTheDom/app.js
@@ -0,0 +1,23 @@
+console.log('Hello');
+const h1 = document.getElementById('main-title');
+
+h1.textContent = "Some new title"
+
+h1.style.color = 'white';
+
+h1.style.backgroundColor = 'black';
+
+const li = document.querySelector('li:last-of-type');
+li.textContent = li.textContent + ' (Changed!)';
+
+const body = document.body;
+
+
+
+//h1.textContent = 'Some new text!';
+
+const listItemElements = document.querySelectorAll('li');
+for (const listItemEl of listItemElements) {
+	console.log(listItemEl);
+	listItemEl.style.color = 'red';
+}
