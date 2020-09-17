bad8a68c82e15b391534367ee25f62585447166b
Yaroslav <boyaal24@gmail.com>
2020-09-17 17:16:40 +0300

    add methods about arrays

--- 09_MoreAboutArrays/app.js
+++ 09_MoreAboutArrays/app.js
@@ -1,4 +1,4 @@
-const numbers = [ 1, 2, 3 ];
+//const numbers = [ 1, 2, 3 ];
 
 /* var arr = [];
 arr[0] = 'a';
@@ -58,10 +58,11 @@ console.log(hobbies);
 const removedElements = hobbies.splice(-2, 1);
 console.log(hobbies); */
 
-const testResult = [ 1, 5.3, 1.5, 10.99, -5, 10 ];
+/* const testResult = [ 1, 5.3, 1.5, 10.99, -5, 10 ];
 
 console.log(testResult.includes(10.99));
 console.log(testResult.indexOf(10.99));
+
 
 //const storedResults = testResult.slice(0, 4);
 const storedResults = testResult.concat([ 3, -2, 5.44 ]);
@@ -79,6 +80,9 @@ const manuel = personalData.find((person, idx, persons) => {
 	return person.name === 'Manuel';
 });
 
+
+
+
 manuel.name = 'Anna';
 
 console.log(manuel, personalData);
@@ -87,4 +91,72 @@ const maxIndex = personalData.findIndex((person, idx, persons) => {
 	return person.name === 'Max';
 });
 
-console.log(maxIndex);
+console.log(maxIndex); */
+
+/* const arr = ['Max', 'Jack', 'Alex'];
+
+const obj = {
+	hello: 'Hello',
+	hi: 'Hi',
+	say() {
+		arr.forEach(function (item) {
+			console.log(this.hello + ' ' + item);
+		  }, this)
+	}
+
+};
+
+obj.say(); */
+
+/* const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+const taxAdjustedPrices = [];
+
+/* for (const price of prices){
+	taxAdjustedPrices.push(Math.round(price * (1 + tax)));
+} */
+
+/* prices.forEach((price, idx, prices) => {
+	const priceObj = {index: idx, taxAdjPrice: Math.round(price * (1 + tax))  };
+	taxAdjustedPrices.push(priceObj);
+});
+
+console.log(taxAdjustedPrices);  */
+
+const prices = [ 10.99, 5.99, 3.99, 6.59 ];
+const tax = 0.19;
+
+/* const taxAdjustedPrices = prices.map((price, idx, prices) => {
+	const priceObj = {
+		index: idx,
+		taxAdjPrice: Math.round(price * (1 + tax))
+	};
+	return priceObj;
+}); */
+
+//console.log(prices, taxAdjustedPrices);
+
+const sortedPrices = prices.sort((a, b) => {
+	if (a > b) {
+		return 1;
+	} else if (a === b) {
+		return 0;
+	} else {
+		return -1;
+	}
+});
+
+//console.log(sortedPrices.reverse());
+
+const filteredArray = prices.filter((price, index, prices) => {
+	return price > 6;
+});
+/* const filteredArray = prices.filter((price, index, prices) => {
+	return price > 6;
+}); */
+
+console.log(filteredArray);
+
+const shortArray = prices.filter(p => p > 3);
+
+console.log(shortArray);
