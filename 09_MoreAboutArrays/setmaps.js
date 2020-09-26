2b80359438b2cd2312a98b627508e0d19ea9cc8b
Yaroslav <boyaal24@gmail.com>
2020-09-26 20:14:13 +0300

    add methods about sets

--- 09_MoreAboutArrays/setmaps.js
+++ 09_MoreAboutArrays/setmaps.js
@@ -1,9 +1,26 @@
-console.log('hi');
+/* console.log('hi');
 
 const ids = new Set([ 'Hi', 'from', 'set!' ]);
+ids.delete('Hi');
 //ids.add(4);
 //console.log(ids.has(1));
 
 for (const entry of ids.entries()) {
+	console.log(entry);
+} */
+
+const person1 = { name: 'Max' };
+
+const person2 = { name: 'Manual' };
+
+const personData = new Map([ [ person1, [ { date: 'yesterday', price: 10 } ] ] ]);
+console.log(personData);
+
+personData.set(person2, [ { date: 'two weeks ago', price: 100 } ]);
+
+console.log(personData);
+console.log(personData.get(person1));
+
+for (const entry of personData.entries()) {
 	console.log(entry);
 }
